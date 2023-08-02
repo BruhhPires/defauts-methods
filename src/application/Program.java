a2552cfbe43b4106903cf10615c268aaf6c2a9b0
Bruno Pires <brunopires.ctto@gmail.com>
2023-08-02 14:48:32 -0300

    Sem Default Methods + Interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 import services.BrazilInterestService;
+import services.InterestRate;
 import services.UsaInterestService;
 
 public class Program {
@@ -11,16 +12,16 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
 		System.out.print("Amount: ");
 		double amount = sc.nextDouble();
 		System.out.print("Months: ");
 		int months = sc.nextInt();
 		
-		UsaInterestService us = new UsaInterestService(1.0);
-		BrazilInterestService br = new BrazilInterestService(2.0);
+		InterestRate in = new BrazilInterestService(2.0);
+		//InterestRate in = new UsaInterestService(1.0);  <-- OU SE DESEJAR MUDAR A CLASSE UTILIZANDO A INTERFACE
 
-		double payment = br.payment(amount, months);
+		
+		double payment = in.payment(amount, months);
 				
 		System.out.println("Payment after: " + months + " months:");
 		System.out.println(String.format("%.2f", payment));
--- src/services/UsaInterestService.java
+++ src/services/UsaInterestService.java
@@ -2,22 +2,20 @@ package services;
 
 import java.security.InvalidParameterException;
 
-public class UsaInterestService {
+public class UsaInterestService implements InterestRate{
 	
 	private double interestRate;
 
 	public UsaInterestService(double interestRate) {
 		this.interestRate = interestRate;
 	}
-
+	
+	@Override
 	public double getInterestRate() {
 		return interestRate;
 	}
-
-	public void setInterestRate(double interestRate) {
-		this.interestRate = interestRate;
-	}
 	
+	@Override
 	public double payment(double amount, int months) {
 		if(months<1) {
 			throw new InvalidParameterException("Months must be greater than zero");
